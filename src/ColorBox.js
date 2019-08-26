2b48a853c9f37c8d27d7946a69f55adffb5c69e9
Nikita Kozlov <yellowswordy@yandex.com>
2019-08-26 10:00:48 -0700

    Add CopyOverlay animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,43 @@
 import React, {Component} from 'react';
 import './ColorBox.css'
-import  { CopyToClipboard } from 'react-copy-to-clipboard'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+
 class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {copied: false};
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+
+    changeCopyState() {
+        this.setState({
+            copied: true
+        }, () => setTimeout(() => this.setState({copied: false}), 1500))
+    }
+
     render() {
         //extracting what in props
         const {name, background} = this.props;
+        const {copied} = this.state;
         return (
-            <CopyToClipboard text={background}>
-                <div style={{background}} className='Color-box'>
+            //to control style we will use callback here
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div style={{background}} className='ColorBox'>
+                    <div
+                        style={{background}}
+                        className={`copy-overlay ${copied && 'show'}`}>
+                    </div>
+                    <div className={`copy-msg ${copied && 'show'}`}>
+                        <h1>coppied!</h1>
+                        <p>{background}</p>
+                    </div>
                     <div className="copy-container">
                         <div className="box-content">
                             <span>{name}</span>
                         </div>
                         <button className='copy-button'>Copy</button>
-                    </div>
-                    <span className='see-more'>More</span>
+
+                        <span className='see-more'>More</span></div>
                 </div>
             </CopyToClipboard>
         );
