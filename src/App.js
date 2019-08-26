a6617d2b30a5d8797ac384d76ba0df3ff3b619f6
Nikita Kozlov <yellowswordy@yandex.com>
2019-08-26 07:26:28 -0700

    Create palette component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Palette from "./Palette";
+import seedColors from './SeedColors'
 
 function App() {
   return (
     <div className="App">
-     <h1>New app</h1>
+        {/*with ... we unpack all values from palette 5th element*/}
+        <Palette palette={...seedColors[5]}/>
     </div>
   );
 }
---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className='Palette'>
+                {/*Navbar goes here*/}
+                <div  className='Palette-colors'>
+                {/*    here color boxes*/}
+                </div>
+            {/*    Footer in future*/}
+            </div>
+        );
+    }
+}
+
+export default Palette;
