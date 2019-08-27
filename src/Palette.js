f07fd930c5bea66737dcd5274d5ad5f09a3d9bb0
Nikita Kozlov <yellowswordy@yandex.com>
2019-08-27 02:51:06 -0700

    Add select and material design

--- src/Palette.js
+++ src/Palette.js
@@ -2,30 +2,39 @@ import React, {Component} from 'react';
 import ColorBox from "./ColorBox";
 import './Palette.css'
 import Navbar from "./Navbar";
+
 class Palette extends Component {
     constructor(props){
         super(props);
         this.state = {
-            level: 500
+            level: 500,
+            format: 'hex'
         };
-        this.changeLevel = this.changeLevel.bind(this)
+        this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     changeLevel(newLevel){
         this.setState({level: newLevel})
 
     }
+    changeFormat(val){
+        this.setState({format:val})
+    }
 
     render() {
         const {colors} = this.props.palette;
-        const {level} = this.state;
+        const {level, format} = this.state;
         //here we render box all box component from individual palette
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name}/>
+            <ColorBox background={color[format]} name={color.name}/>
         ));
         return (
             <div className='Palette'>
-                <Navbar level={level} changeLevel={this.changeLevel}/>
+                <Navbar
+                    level={level}
+                    changeLevel={this.changeLevel}
+                    handleChange={this.changeFormat}/>
                 {/*Navbar goes here*/}
                 <div  className='Palette-colors'>
                 {colorBoxes}
