476ca93bf52c16c0a6c74f37a4cd0c50b40ad97a
Nikita Kozlov <yellowswordy@yandex.com>
2019-08-26 22:43:37 -0700

    Add unstyled slider to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Palette from "./Palette";
 import seedColors from './SeedColors'
 import {generatePalette} from "./colorHelpers";
 
 function App() {
-    console.log(generatePalette(seedColors[4]));
+
   return (
     <div className="App">
         {/*with ... we unpack all values from palette 5th element*/}
-        <Palette {...seedColors[5]}/>
+        <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }
