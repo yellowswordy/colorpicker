78b4494018ac507572f539d6efa455327fa7ad7d
Nikita Kozlov <yellowswordy@yandex.com>
2019-08-27 06:41:34 -0700

    Set Plette rout using id

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import Palette from "./Palette";
 import seedColors from './SeedColors'
 import {generatePalette} from "./colorHelpers";
 
-function App() {
+class App extends Component {
+    findPalette(id) {
+        return seedColors.find(function (palette) {
+            return palette.id === id
+        });
+    }
 
-  return (
-        <Switch>
-            <Route exact path='/' render={() => <h1>Palette list ges here</h1>}/>
-            <Route exact path='/palette/:id' render={() => <h1>Individual palette</h1>}/>
-        </Switch>
-    // <div className="App">
-    //     {/*with ... we unpack all values from palette 5th element*/}
-    //     <Palette palette={generatePalette(seedColors[4])}/>
-    // </div>
-  );
+    render() {
+
+
+        return (
+            <Switch>
+                <Route exact path='/' render={() => <h1>Palette list ges here</h1>}/>
+                <Route exact path='/palette/:id' render={routeProps => <Palette
+                    palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}/>
+            </Switch>
+            // <div className="App">
+            //     {/*with ... we unpack all values from palette 5th element*/}
+            //     <Palette palette={generatePalette(seedColors[4])}/>
+            // </div>
+        );
+    }
 }
 
 export default App;
