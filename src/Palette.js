5ec9bc9ec69e3c691a4ebd88aaf9f7c0329eb121
Nikita Kozlov <yellowswordy@yandex.com>
2019-09-09 02:23:45 -0700

    move out stuyles

--- src/Palette.js
+++ src/Palette.js
@@ -2,19 +2,11 @@ import React, {Component} from 'react';
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
-import './Palette.css'
-import {withStyles} from "@material-ui/styles";
 
-const styles = {
-    Palette: {
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    colors: {
-        height: '90%'
-    }
-}
+import {withStyles} from "@material-ui/styles";
+import styles from './styles/PaletteStyles'
+
+
 
 class Palette extends Component {
     constructor(props) {
---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,14 @@
+export default {
+    PaletteFooter: {
+        backgroundColor: 'white',
+        height: '5vh',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        fontweight: 'bold',
+    },
+    empji: {
+        fontSize: '1.5rem',
+        margin: '0 1rem',
+    }
+};
