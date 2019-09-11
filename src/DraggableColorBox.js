125796d4ba09db484a10a0130c22122dcf57c6e3
Nikita Kozlov <yellowswordy@yandex.com>
2019-09-11 04:03:56 -0700

    Implement Drag and Drop

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 import DeleteIcon from '@material-ui/icons/Delete';
+import { SortableElement }from 'react-sortable-hoc'
 const styles = {
     root: {
         width: "20%",
@@ -34,7 +35,7 @@ const styles = {
     }
 };
 
-function DraggableColorBox(props) {
+const DraggableColorBox = SortableElement((props) =>{
     const { classes, handleClick, name, color } = props;
     return (
         <div
@@ -48,5 +49,5 @@ function DraggableColorBox(props) {
             
         </div>
     );
-}
+})
 export default withStyles(styles)(DraggableColorBox);
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -14,6 +14,9 @@ import Button from "@material-ui/core/Button";
 import DraggableColorBox from "./DraggableColorBox";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { ChromePicker } from "react-color";
+import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import DraggableColorList from './DraggableColorList';
+import {arrayMove} from "react-sortable-hoc";
 
 const drawerWidth = 400;
 
@@ -88,6 +91,7 @@ class NewPaletteForm extends Component {
         this.addNewColor = this.addNewColor.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.removeColor = this.removeColor.bind(this);
     }
     componentDidMount() {
         ValidatorForm.addValidationRule("isColorNameUnique", value =>
@@ -137,6 +141,11 @@ class NewPaletteForm extends Component {
             colors: this.state.colors.filter(color => color.name !== colorName )
         })
     }
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.setState(({colors}) => ({
+            colors: arrayMove(colors, oldIndex, newIndex),
+        }));
+    };
 
     render() {
         const { classes } = this.props;
@@ -228,9 +237,12 @@ class NewPaletteForm extends Component {
                     })}
                 >
                     <div className={classes.drawerHeader} />
-                    {this.state.colors.map(color => (
-                        <DraggableColorBox key={color.key} color={color.color} name={color.name} handleClick={() => this.removeColor(color.name)}/>
-                    ))}
+                    <DraggableColorList
+                        onSortEnd={this.onSortEnd}
+                        axis="xy"
+                        colors={this.state.colors}
+                        removeColor={this.removeColor}
+                    />
                 </main>
             </div>
         );
