1fb720784a54455ec841dbec430888cdb3a94fe9
Nikita Kozlov <yellowswordy@yandex.com>
2019-09-10 19:52:32 -0700

    Add delete to PaletteForm using name instead ID

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -132,6 +132,11 @@ class NewPaletteForm extends Component {
         this.props.savePalette(newPalette);
         this.props.history.push("/");
     }
+    removeColor(colorName){
+        this.setState({
+            colors: this.state.colors.filter(color => color.name !== colorName )
+        })
+    }
 
     render() {
         const { classes } = this.props;
@@ -224,7 +229,7 @@ class NewPaletteForm extends Component {
                 >
                     <div className={classes.drawerHeader} />
                     {this.state.colors.map(color => (
-                        <DraggableColorBox color={color.color} name={color.name} />
+                        <DraggableColorBox key={color.key} color={color.color} name={color.name} handleClick={() => this.removeColor(color.name)}/>
                     ))}
                 </main>
             </div>
