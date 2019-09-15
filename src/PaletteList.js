dd55dc96fe01359a38ec3bbccbfba5087907584d
Nikita Kozlov <yellowswordy@yandex.com>
2019-09-15 05:33:30 -0700

    Refactor with PureComponent to prevent rerender

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -29,6 +29,7 @@ class PaletteList extends Component {
         this.closeDialog = this.closeDialog.bind(this);
         this.openDialog = this.openDialog.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.goToPalette = this.goToPalette.bind(this);
     };
 
     openDialog(id) {
@@ -63,7 +64,7 @@ class PaletteList extends Component {
                             <CSSTransition key={palette.id} classNames='fade' timeout={500}>
                                 <MiniPalette
                                     {...palette}
-                                    handleClick={() => this.goToPalette(palette.id)}
+                                    goToPalette={this.goToPalette}
                                     // handleDelete={deletePalette}
                                     openDialog={this.openDialog}
                                     key={palette.id}
